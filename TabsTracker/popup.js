1ab556f7b97bc88dcf8369fcccbd89f160aebb4e
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-06-16 09:50:23 +0530

    update all files

--- TabsTracker/popup.js
+++ TabsTracker/popup.js
@@ -4,10 +4,22 @@ chrome.storage.local.get('productivityData', function(result) {
 
   const websiteList = document.getElementById('websiteList');
 
+  document.getElementById('clearTabsButton').addEventListener('click', function() {
+    chrome.storage.local.set({ productivityData: {} }); // Clear the stored data
+    const taskList = document.getElementById('websiteList')
+    taskList.innerHTML = ''
+  });
+  
+  
+
   for (const [website, timeSpent] of Object.entries(productivityData)) {
     const listItem = document.createElement('li');
-    listItem.textContent = `${website}: ${timeSpent} minutes`;
-
+    const websiteName = website.replace(/^(https?:\/\/)?(www\.)?([^/]+)\..+$/i, '\$3'); // Extract the third capture group from the regular expression
+    timeText = timeSpent > 1 ? 'minutes' : 'minute'
+    listItem.textContent = `${websiteName}: ${timeSpent} ${timeText}`;
+    
     websiteList.appendChild(listItem);
   }
+
+
 });
